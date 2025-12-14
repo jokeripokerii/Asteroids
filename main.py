8c601c772936184e272cff11187435811953f0a3
jokeripokerii <jori.toivanen1@gmail.com>
2025-12-14 13:25:19 +0200

    shooting and deleting features

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
+import sys
 from circleshape import CircleShape
 from player import Player
+from asteroid import Asteroid
+from shot import Shot
+from asteroidfield import AsteroidField
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 
 def main():
     pygame.init()
@@ -10,20 +14,55 @@ def main():
     kello = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    #all groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    #containers
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
+    #setting up
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     pelaaja = Player(x, y)
+    asteroidikentta = AsteroidField()
 
+    #game loop
     while True:
         log_state()
 
+        #exit button
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        #drawing objects
         screen.fill("black")
-        pelaaja.draw(screen)
-        pelaaja.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+
+        #update objects
+        updatable.update(dt)
+
+        #checking player collision to asteroids
+        for roid in asteroids:
+            if roid.collides_with(pelaaja) == True:
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        for roid2 in asteroids:
+            for buret in shots:
+                if roid2.collides_with(buret) == True:
+                    log_event("asteroid_shot")
+                    roid2.kill()
+                    buret.kill()
+
         pygame.display.flip()
         kello.tick(60)
         dt = kello.tick(60) / 1000
--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 import pygame
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import *
 from circleshape import CircleShape
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.cooldown = 0
 
     # in the Player class
     def triangle(self):
@@ -24,8 +26,29 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.cooldown -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            if self.cooldown > 0:
+                pass
+            else:
+                self.shoot()
+                self.cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
+    
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+
+    def shoot(self):
+        paukku = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        paukku.velocity = pygame.math.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
